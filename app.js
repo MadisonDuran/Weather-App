c871b59c7173db5b9d00f722a7a90d882b09e9cd
Madison Duran <madisonduran74@gmail.com>
2025-08-26 20:16:44 -0400

    Made a http request.

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const url = 'https://api.weatherstack.com/current?access_key=0ec0cc9c5063651eb9c
 request({ url: url, json: true }, (error, response) => {
    console.log(response.body.current.weather_descriptions[0]  + ". It is currently " + response.body.currently + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1ijo1W5kcmV3bWhZDEiLCJhIjoiY2pzbml2OG9jMGN2MTQ0cGd3bVheTFUay"
+
+request({ url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
+})
